20ae665c42a790f5e841fe319748f2459061b930
Kolonya <50084113+Kolonyaa@users.noreply.github.com>
2025-10-23 01:27:18 +0200

    NEW9

--- plugins/obfuscation/src/index.ts
+++ plugins/obfuscation/src/index.ts
@@ -1,24 +1,20 @@
 import { applyPatches } from "./patcher";
 import applyPillPatcher from "./pillPatcher";
-import applyAttachmentPatcher from "./attachmentPatcher";
 import Settings from "./Settings";
 
 let unpatch: () => void;
 let pillUnpatch: () => void;
-let attachmentUnpatch: () => void;
 
 export function onLoad() {
   // Initialize all patchers
   unpatch = applyPatches();
   pillUnpatch = applyPillPatcher?.();
-  attachmentUnpatch = applyAttachmentPatcher?.();
 }
 
 export function onUnload() {
   // Safely call all unpatch functions
   unpatch?.();
   pillUnpatch?.();
-  attachmentUnpatch?.();
 }
 
 export const settings = Settings;
